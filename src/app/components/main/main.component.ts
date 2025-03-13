1f4365e5dacbaf929b3adb4d559e22b7883b3e80
JonArbell <138678861+JonArbell@users.noreply.github.com>
2025-03-13 10:55:51 +0800

    First commit

---
+++ src/app/components/main/main.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  imports: [],
+  templateUrl: './main.component.html'
+})
+export class MainComponent {
+  
+  hiringManagerName : string = "[Hiring Manager's Name]";
+  companyName : string = "[Company Name]";
+  companyAddress : string = "[Company Address]";
+
+  jobTitle : string = "[Job Title]";
+
+
+
+}
